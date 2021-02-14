77dcb41fc733c67049967068d12bfb49f4842afd
harvey magana <heyharv2012@gmail.com>
2021-02-14 15:15:34 -0800

    Updated the auth-router file and the seeds file for the users.

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,7 +1,7 @@
 const bcrypt = require("bcrypt");
 const router = require("express").Router();
-//const jwt = require('jsonwebtoken');
-//const secrets = require('../config/secrets.js');
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets.js');
 
 const Users = require("../users/users-model.js");
 const { isValid } = require("../users/users-service.js");
@@ -17,5 +17,44 @@ router.post("/register", (req, res) => {
       res.status(500).json({ message: error.message });
     });
   });
+
+  router.post("/login", (req, res) => {
+    const { username, password } = req.body;
+  
+    if (isValid(req.body)) {
+      Users.findBy({ username: username })
+        .then(([user]) => {
+          // compare the password the hash stored in the database
+          if (user && bcrypt.compareSync(password, user.password)) {
+            const token = generateToken(user);
+            res.status(200).json({ message: "Welcome to our API", token });
+          } else {
+            res.status(401).json({ message: "Invalid credentials" });
+          }
+        })
+        .catch(error => {
+          res.status(500).json({ message: error.message });
+        });
+    } else {
+      res.status(400).json({
+        message: "please provide username and password and the password shoud be alphanumeric",
+      });
+    }
+  });
+
+  function generateToken(user) {
+    const payload = {
+      subject: user.id,
+      username: user.username,
+      department: user.department
+    }
+  
+    const options = {
+      expiresIn: '1d'
+    }
+  
+    const token = jwt.sign(payload, secrets.jwtSecret, options);
+    return token;
+  }
 
   module.exports = router;
--- database/seeds/01-init.js
+++ database/seeds/01-init.js
@@ -3,7 +3,8 @@
 exports.seed = async function(knex) {
     return knex('users')
     .insert([
-        {id:3, username:"testuser2", password:"password1234", department: "admin"}
+        {id:1, username:"testuser2", password:"password1234", department: "admin"},
+        {id:2, username:"testuser3", password:"password5678", department: "staff"}
       ])
 }
 
